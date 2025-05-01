0e44a739f22feac96452aac534c0cfc8d88f0afa
Sagar Satyarthi Mishra <sagarsatyarthimishra@gmail.com>
2025-05-01 10:10:58 +0530

    update login page

--- excle-analytics/src/Components/Login.jsx
+++ excle-analytics/src/Components/Login.jsx
@@ -1,17 +1,59 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      <form>
-        <label htmlFor="username">Username: </label>
-        <input type="text" id="username" name="username" required />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" required />
-        <br />
-        <button type="submit">Login</button>
-        <button type="reset">Reset</button>
+    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <form
+        className="bg-amber-800 text-xl text-white rounded shadow-md w-96 h-90 text-center flex flex-col px-4  pb-2pt-4 pb-2 justify-between hover:scale-110"
+        onSubmit={(e) => {e.preventDefault(); setPassword(""); setUsername("");}}
+      >
+        <h1 className="text-2xl font-bold mb-4 border-b-1 inline">Login</h1>
+        <div className="flex flex-col gap-4 mb-4">
+          <div>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your username"
+              className="border-2 rounded-s hover:bg-amber-700"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="border-2 rounded-s ml-1.5 hover:bg-amber-700"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-center gap-5  items-center group">
+            <button
+              type="submit"
+              className="border-2 px-4 py-0.5 cursor-pointer bg-amber-700 border-amber-200 hover:bg-cyan-700 focus:bg-green-900"
+            >
+              Login
+            </button>
+            <button
+              type="reset"
+              className="text-blue-500 cursor-pointer border-b-1 border-blue-500"
+            >
+              Reset
+            </button>
+          </div>
+          <p className="text-base mt-1">Create new Account</p>
+        </div>
       </form>
     </div>
   );
